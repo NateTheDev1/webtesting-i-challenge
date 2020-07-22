0485e8121728efa009606b0cb06d21799da74d53
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-22 13:01:46 -0400

    Repair Working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const item = {
+  durability: 35,
+};
+
+describe("repair", () => {
+  it("can repair an items durability to 100", () => {
+    const expectedOutput = 100;
+    const actualOutput = enhancer.repair(item);
+    expect(actualOutput.durability).toBe(expectedOutput);
+  });
+});
