1c517494a67a2e6760f7cf4316f1b1e4aba38f8b
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-22 13:18:12 -0400

    Fail Passing All

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,13 +1,13 @@
 const enhancer = require("./enhancer.js");
 // test away!
 
-const item = {
-  name: "Item 1",
-  enhancement: 10,
-  durability: 35,
-};
-
 describe("repair", () => {
+  const item = {
+    name: "Item 1",
+    enhancement: 10,
+    durability: 35,
+  };
+
   it("can repair an items durability to 100", () => {
     const expectedOutput = 100;
     const actualOutput = enhancer.repair(item);
@@ -16,11 +16,17 @@ describe("repair", () => {
 });
 
 describe("success", () => {
+  const item = {
+    name: "Item 1",
+    enhancement: 10,
+    durability: 35,
+  };
+
   it("can enhance an item by 1", () => {
     const expectedOutput = 11;
     const actualOutput = enhancer.success(item);
     expect(actualOutput.enhancement).toBe(expectedOutput);
-    expect(actualOutput.durability).toBe(100);
+    expect(actualOutput.durability).toBe(35);
   });
 
   it("will not enhance an item already at the max enhancement", () => {
@@ -33,5 +39,40 @@ describe("success", () => {
     const expectedOutput = 20;
     const actualOutput = enhancer.success(maxEnhancement);
     expect(actualOutput.enhancement).toBe(expectedOutput);
+  });
+});
+
+describe("fail", () => {
+  it("will decrease by 5 if enhancement is less than 15", () => {
+    const item = {
+      name: "Item 1",
+      enhancement: 10,
+      durability: 35,
+    };
+    const expected = 5;
+    const actual = enhancer.fail(item);
+    expect(actual.enhancement).toBe(expected);
+  });
+
+  it("will decrease by 1 if enhancement is greater than 16", () => {
+    const item = {
+      name: "Item 1",
+      enhancement: 17,
+      durability: 35,
+    };
+    const expected = 16;
+    const actual = enhancer.fail(item);
+    expect(actual.enhancement).toBe(expected);
+  });
+
+  it("will decrease by 10 if enhancement is greater than 15 and less than 16", () => {
+    const item = {
+      name: "Item 1",
+      enhancement: 16,
+      durability: 35,
+    };
+    const expected = 6;
+    const actual = enhancer.fail(item);
+    expect(actual.enhancement).toBe(expected);
   });
 });
