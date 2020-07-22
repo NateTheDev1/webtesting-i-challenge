312d01bcbdbad3aa67ae556c0de05f27d7e298cb
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-22 13:52:15 -0400

    Started working on ui

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <h1>Inventory</h1>
+      <div className="functions">
+        <button className="btn-1">Repair</button>
+        <button className="btn-2">Enhance</button>
+      </div>
     </div>
   );
 }
